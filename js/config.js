c00c30cdfd59120c0be57619760668fdb677ece8
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-12-02 21:23:38 +0100

    addInvokeURL

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
     region: "eu-west-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    invokeUrl: "https://9s746pw42j.execute-api.eu-west-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
   },
 };
