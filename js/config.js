ef744aacbf08afbadd891c5d18c9b15451c2aaef
YAlorenzo <zhenia.klimenchuk@gmail.com>
2023-12-02 20:39:01 +0100

    fix_config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
   cognito: {
-    userPoolId:
-      "arn:aws:cognito-idp:eu-west-1:600756373662:userpool/eu-west-1_hhH0B883p", // e.g. us-east-2_uXboG5pAb
-    userPoolClientId: "4lsahf3782t4nj1cnf9voeile3", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    userPoolId: "eu-west-1_IbgkN68Qy", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "29ijbh0asidmtbpissrlnfecrl", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
     region: "eu-west-1", // e.g. us-east-2
   },
   api: {
